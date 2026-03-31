c76f30fa9cc785c21c658190f46cb7fb95971e90
afmnasiruddin01 <afmnasiruddin01@gmail.com>
2026-03-31 21:32:46 +0600

    res.json() updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 var router = Router();
 
-/* GET home page. */
+/* GET API home. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.json({
+    message: "Express API is running",
+    title: "Express",
+  });
 });
 
 export default router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  res.json({ message: "respond with a resource" });
 });
 
 export default router;
